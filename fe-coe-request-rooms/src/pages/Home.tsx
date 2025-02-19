326e875c445cb08c9d680f27933d7bfc0fba5758
WeerapongKh <weerapong.k@kkumail.com>
2025-02-19 22:50:15 +0700

    feat(welcome-page): implemented the welcome-page ui

--- fe-coe-request-rooms/src/pages/Home.tsx
+++ fe-coe-request-rooms/src/pages/Home.tsx
@@ -1,10 +1,30 @@
+import ImgWelcome from '../assets/image-welcome-page.png';
+import Button from '../components/Button';
 
 const HomePage = () => {
-  return (
-    <div>
-        <h1>Coe Rooms</h1>
-    </div>
-  )
-}
+    return (
+        <div className="w-screen h-screen bg-[var(--background-color)] flex flex-col items-center">
+            <h1 className="text-6xl font-bold text-[var(--text-color)] my-14">
+                <span className="text-[var(--primary-color)]">CoE</span> Rooms
+            </h1>
+
+            <Button>
+                <a href="#">Sign in</a>
+            </Button>
+
+            <p className="mt-8 text-[var(--text-color)]">
+                You don’t have an account?{' '}
+                <span className="text-[var(--primary-color)] cursor-pointer">Sign up</span>
+            </p>
+
+            <img className="mt-14" src={ImgWelcome} alt="Welcome Illustration" />
+
+            <footer className="mt-14 text-center text-[#140B0C] opacity-40">
+                <p>Developed by</p>
+                <p>BOSS OHM LEE EARTH ICE</p>
+            </footer>
+        </div>
+    );
+};
 
 export default HomePage;
