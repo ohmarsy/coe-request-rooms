825e455b22a260198f42aa045ccfadfc384fe9b4
ohmarsy <suphachai.y@kkumail.com>
2025-02-19 16:53:30 +0700

    init tailwindcss

--- fe-coe-request-rooms/src/App.tsx
+++ fe-coe-request-rooms/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="font-bold text-2xl text-red-300">
         Click on the Vite and React logos to learn more
       </p>
     </>
