835d45d1d76f14c968dcf95fcbb9f380d594437a
ohmarsy <suphachai.y@kkumail.com>
2025-02-21 16:37:07 +0700

    setting to use main page with sidebar

--- fe-coe-request-rooms/src/pages/Main.tsx
+++ fe-coe-request-rooms/src/pages/Main.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import SideBar from "../layout/SideBar";
+import Dashboard from "./Dashboard";
 
 const MainPage = () => {
   return (
     <div className="flex">
-
       <SideBar></SideBar>
       <div className="p-7 text-2xl font-bold flex-1 h-screen">
-        <h1> MainPage</h1>
+        <Dashboard />
       </div>
-      
     </div>
   );
 };
