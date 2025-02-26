68c3ecead018956dee3153ba08bb8758cad8b3e9
JakkapatB <jakkapat.bo@kkumail.com>
2025-02-26 16:48:44 +0700

    customize switch

--- fe-coe-request-rooms/src/components/Switch.tsx
+++ fe-coe-request-rooms/src/components/Switch.tsx
@@ -2,26 +2,35 @@ import React, { useState } from 'react'
 interface SwitchProps {
     leftname: string
     rightname: string
-    onClick_left?: () => void
-    onClick_right?: () => void
+    onClick_left: () => void
+    onClick_right: () => void
 }
-const Switch: React.FC<SwitchProps> = ({ leftname, rightname }) => {
+const Switch: React.FC<SwitchProps> = ({ leftname, rightname, onClick_left, onClick_right }) => {
     const [active, setActive] = useState<"leftfunc" | "rigthfunc">("leftfunc");
+    const handleLeft = () => {
+        setActive("leftfunc")
+        onClick_left()
+    }
+    const handleRight = () => {
+        setActive("rigthfunc")
+        onClick_right()
+
+    }
     return (
         <div className='flex p-4'>
             <div
                 className='flex bg-[var(--background-color)] rounded-full border-1 border-gray-200'>
                 <button
-                    onClick={() => setActive("leftfunc")}
-                    className={`px-4 py-2 rounded-l-full text-base transition ${active === "leftfunc"
+                    onClick={() => handleLeft()}
+                    className={`px-4 py-2 rounded-l-full text-sm transition ${active === "leftfunc"
                         ? "bg-[var(--primary-color)] text-white"
-                        : "text-[var(--text-color)] cursor-pointer" 
+                        : "text-[var(--text-color)] cursor-pointer"
                         }`}>
                     {leftname}
                 </button>
                 <button
-                    onClick={() => setActive("rigthfunc")}
-                    className={`px-4 py-2 rounded-r-full text-base  transition ${active === "rigthfunc"
+                    onClick={() => handleRight()}
+                    className={`px-4 py-2 rounded-r-full text-sm  transition ${active === "rigthfunc"
                         ? "bg-[var(--primary-color)] text-white"
                         : "text-[var(--text-color)] cursor-pointer"
                         }`}>
--- fe-coe-request-rooms/src/pages/RequestRooms.tsx
+++ fe-coe-request-rooms/src/pages/RequestRooms.tsx
@@ -2,18 +2,31 @@ import React from 'react'
 import Navbar from '../layout/Navbar'
 import Switch from '../components/Switch'
 
-const RequestRoomsPage = () => {
+
+
+const RequestRooms = () => {
+    const [activeComponent, setActiveComponent] = React.useState<"RequestRoom" | "History">("RequestRoom");
+    const handleLeft = () => {
+        setActiveComponent("RequestRoom")
+        console.log('left')
+    }
+    const handleRight = () => {
+        setActiveComponent("History")
+        console.log('right')
+    }
     return (
         <div>
             <Navbar name={'Request rooms'} />
-            <div className='flex flex-col items-center justify-center space-y-4 mt-8'>
-                <Switch leftname={'Request Rooms'} rightname={'Request History'} />
-                <h1 className='text-xl font-medium'>Request to join rooms</h1>
-                <div className='flex flex-row'>
-                    <div className='flex-1'>
-
+            <div className='flex flex-col items-center justify-center space-y-8 mt-8'>
+                <Switch leftname={'Request Rooms'} rightname={'Request History'} onClick_left={handleLeft} onClick_right={handleRight} />
+                <h1 className='text-xl font-medium'>{activeComponent === 'RequestRoom' ? 'Request to join rooms' : 'Request History'}</h1>
+                <div className='flex flex-row w-full px-8 space-x-8'>
+                    <div className='flex-1 flex flex-col'>
+                        <p className='text-center text-lg font-medium'>Room can join</p>
+                        <div className='flex flex-col space-y-4'>
+                        </div>
                     </div>
-                    <div className='flex-1'>
+                    <div className='flex-3 bg-yellow-500'>
 
                     </div>
                 </div>
@@ -22,4 +35,4 @@ const RequestRoomsPage = () => {
     )
 }
 
-export default RequestRoomsPage
+export default RequestRooms
