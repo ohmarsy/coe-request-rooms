bbfe52faa4a24b06bca15f81d98e5d3318ca1c2a
JakkapatB <jakkapat.bo@kkumail.com>
2025-02-26 15:53:23 +0700

    fix font size and and have cursor pointer

--- fe-coe-request-rooms/src/components/Switch.tsx
+++ fe-coe-request-rooms/src/components/Switch.tsx
@@ -8,22 +8,22 @@ interface SwitchProps {
 const Switch: React.FC<SwitchProps> = ({ leftname, rightname }) => {
     const [active, setActive] = useState<"leftfunc" | "rigthfunc">("leftfunc");
     return (
-        <div className='flex  p-4'>
+        <div className='flex p-4'>
             <div
                 className='flex bg-[var(--background-color)] rounded-full border-1 border-gray-200'>
                 <button
                     onClick={() => setActive("leftfunc")}
-                    className={`px-4 py-2 rounded-l-full  transition ${active === "leftfunc"
+                    className={`px-4 py-2 rounded-l-full text-sm transition ${active === "leftfunc"
                         ? "bg-[var(--primary-color)] text-white"
-                        : "text-[var(--text-color)]"
+                        : "text-[var(--text-color)] cursor-pointer" 
                         }`}>
                     {leftname}
                 </button>
                 <button
                     onClick={() => setActive("rigthfunc")}
-                    className={`px-4 py-2 rounded-r-full transition ${active === "rigthfunc"
+                    className={`px-4 py-2 rounded-r-full text-sm  transition ${active === "rigthfunc"
                         ? "bg-[var(--primary-color)] text-white"
-                        : "text-[var(--text-color)]"
+                        : "text-[var(--text-color)] cursor-pointer"
                         }`}>
                     {rightname}
                 </button>
--- fe-coe-request-rooms/src/pages/RequestRooms.tsx
+++ fe-coe-request-rooms/src/pages/RequestRooms.tsx
@@ -6,8 +6,17 @@ const RequestRooms = () => {
     return (
         <div>
             <Navbar name={'Request rooms'} />
-            <div className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col items-center justify-center space-y-4 mt-8'>
                 <Switch leftname={'Request Rooms'} rightname={'Request History'} />
+                <h1 className='text-xl font-medium'>Request to join rooms</h1>
+                <div className='flex flex-row'>
+                    <div className='flex-1'>
+
+                    </div>
+                    <div className='flex-1'>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
