d0e3d969479742586a7f7b138fbae93dcb5582ac
ohmarsy <suphachai.y@kkumail.com>
2025-03-03 11:27:47 +0700

    style: max-h for image to make dashboard beautiful layout

--- fe-coe-request-rooms/src/components/Card.tsx
+++ fe-coe-request-rooms/src/components/Card.tsx
@@ -1,31 +1,32 @@
-import React from 'react'
+import React from "react";
 
 // Define the type for the props
 interface CardProps {
-    image: string; // URL of the image
-    name: string;  // Name to display as the title
-    email: string; // Email to display in the description
+  image: string; // URL of the image
+  name: string; // Name to display as the title
+  email: string; // Email to display in the description
 }
 
 const Card: React.FC<CardProps> = ({ image, name, email }) => {
-    return (
-       
-        <div className=' bg-white flex flex-col rounded-2xl shadow-sm overflow-hidden md:w-full h-full md:max-h-xs'>
-            <div className=' h-3/5'>
-                <img
-                    className='h-full w-full object-cover'
-                    src={image}
-                    alt={`${name}'s profile`}
-                />
-
-            </div>
-            <div className=' h-2/5 flex flex-col justify-center items-center'>
-            <h2 className="antialiased mx-auto text-sm sm:text-base font-bold text-gray-600">Name : {name}</h2> 
-               <p className="mx-auto p-2 text-sm text-clip text-gray-600 mt-2">Email : {email}</p> 
-            </div>
-
-        </div>
-    );
+  return (
+    <div className=" bg-white flex flex-col rounded-2xl shadow-sm overflow-hidden md:w-full h-full md:max-h-xs">
+      <div className="flex h-3/5">
+        <img
+          className="max-h-45 w-full object-cover"
+          src={image}
+          alt={`${name}'s profile`}
+        />
+      </div>
+      <div className="h-2/5 flex flex-col justify-center items-center">
+        <h2 className="antialiased mx-auto text-sm sm:text-base font-bold text-gray-600">
+          Name : {name}
+        </h2>
+        <p className="mx-auto p-2 text-sm text-clip text-gray-600 mt-2">
+          Email : {email}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
