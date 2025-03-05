c3f014a3fe0b7c29e196124dabaf7ac074fcc3e0
ohmarsy <suphachai.y@kkumail.com>
2025-03-05 18:35:50 +0700

    fix: fix the cors sequence line of code

--- rooms-service/app.py
+++ rooms-service/app.py
@@ -15,14 +15,14 @@ app = Flask(__name__)
 
 load_dotenv()
 
-CORS(app)
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:password@localhost:5433/roomdb')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+CORS(app)
 
 AUTH_SERVICE_URL = "http://localhost:5002" 
 
