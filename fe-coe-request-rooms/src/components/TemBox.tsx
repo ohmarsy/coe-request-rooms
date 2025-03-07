a2567f11fccd377600773bfa9a6e36a92bb8af16
Wachiraya  Phonsa <wachiraya@Wachirayas-MacBook-Air.local>
2025-03-07 15:26:08 +0700

    responsive TemBox

--- fe-coe-request-rooms/src/components/TemBox.tsx
+++ fe-coe-request-rooms/src/components/TemBox.tsx
@@ -10,17 +10,17 @@ const TemperatureDisplay: React.FC<Temperature> = ({ indoor, outdoor, tempType }
   const [unit] = useState('C');
 
   return (
-    <div className="flex flex-col items-center justify-center bg-white  shadow-sm rounded-2xl p-4 w-full h-full">
-      <div className="flex items-start">
-        <span className="text-6xl font-bold text-black">
+    <div className="flex flex-col items-center justify-center bg-white  shadow-sm rounded-2xl p-4 w-full h-full text-sm sm:text-base">
+      <div className="flex items-center ">
+        <span className="text-2xl sm:text-5xl font-bold text-black">
           {indoor ? indoor : outdoor}
         </span>
-        <span className="text-4xl font-bold text-gray-400 mt-2">
+        <span className="text-base sm:text-3xl font-bold text-gray-400">
           °{unit}
         </span>
       </div>
-      <div className="font-medium text-base text-[#7d7d7d] mt-2">
-        {tempType === 'indoor' ? 'Indoor' : 'Outdoor'}
+      <div className="text-sm sm:text-base text-[#7d7d7d] mt-2">
+        {tempType === 'indoor' ? 'Indoor Temp' : 'Outdoor Temp'}
       </div>
     </div>
   );
