751c901d98ff6e17e1d65b40691dfb18d36a6777
WeerapongKh <weerapong.k@kkumail.com>
2025-03-10 22:23:02 +0700

    style(AuthLayout): update responsive and set min-h-screen

--- fe-coe-request-rooms/src/layout/AuthLayout.tsx
+++ fe-coe-request-rooms/src/layout/AuthLayout.tsx
@@ -1,18 +1,19 @@
 import { motion } from "framer-motion";
-import ImageAuth from '../assets/image-auth.png'
+import ImageAuth from '../assets/image-auth.png';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex h-screen px-8 md:px-0 bg-[var(--background-color)]">
-            <div className="hidden overflow-hidden md:flex w-1/3 bg-[var(--primary-color)] text-white flex-col items-center justify-center px-4">
+        <div className="flex min-h-screen px-8 md:px-0 bg-[var(--background-color)]">
+            {/* Sidebar for Auth */}
+            <div className="hidden md:flex w-1/3 bg-[var(--primary-color)] text-white flex-col items-center justify-center px-4">
                 <motion.div
                     initial={{ x: -100, opacity: 0 }}
                     animate={{ x: 0, opacity: 1 }}
                     transition={{ duration: 0.8 }}
-                    className="mt-8 text-base">
-                    <p> CoE Rooms</p>
-                    <p className="text-2xl">
-
+                    className="mt-8 text-base text-center"
+                >
+                    <p className="font-bold text-lg text-start">CoE Rooms</p>
+                    <p className="text-2xl text-start mt-4">
                         Your workspace awaits <br />
                         Where Ideas bloom <br />
                         and comfort meets <br />
@@ -24,11 +25,16 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
                     initial={{ x: -100, opacity: 0 }}
                     animate={{ x: 0, opacity: 1 }}
                     transition={{ duration: 1 }}
-                    className="w-sm  px-8"
-                    src={ImageAuth}></motion.img>
-
+                    className="mt-4 w-3/4 max-w-md" // Adjusted for better responsiveness
+                    src={ImageAuth}
+                    alt="Illustration for Auth"
+                />
             </div>
-            <div className="w-full md:w-2/3">{children}</div>
+
+            {/* Main Content Area */}
+            <div className="w-full md:w-2/3 flex items-center justify-center">
+                {children}
+            </div>
         </div>
     );
 }
