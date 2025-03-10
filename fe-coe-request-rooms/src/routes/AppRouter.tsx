8a58d74f0dbfa0042d08d9a530ab23520d26746c
WeerapongKh <weerapong.k@kkumail.com>
2025-03-10 22:16:51 +0700

    feat(router): implement routing with protected routes

--- fe-coe-request-rooms/src/routes/AppRouter.tsx
+++ fe-coe-request-rooms/src/routes/AppRouter.tsx
@@ -1,21 +1,38 @@
+// AppRouter.tsx
 import { Route, Routes, Navigate } from "react-router-dom";
 import WelcomePage from "../pages/Welcome";
 import SignUpPage from "../pages/SignUp";
 import SignInPage from "../pages/SignIn";
 import MainPage from "../pages/Main";
 import RequestRoomsPage from "../pages/RequestRooms";
+import ProtectedRoute from "../components/ProtectedRoute"; // นำเข้า ProtectedRoute
 const AppRouter = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<WelcomePage/>} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/request-rooms" element={<RequestRoomsPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/sign-in" element={<SignInPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
+      
+      {/* ใช้ ProtectedRoute สำหรับหน้า Main และ RequestRooms */}
+      <Route 
+        path="/main" 
+        element={
+          <ProtectedRoute>
+            <MainPage />
+          </ProtectedRoute>
+        } 
+      />
+      <Route 
+        path="/request-rooms" 
+        element={
+          <ProtectedRoute>
+            <RequestRoomsPage />
+          </ProtectedRoute>
+        } 
+      />
+      
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 };
 
