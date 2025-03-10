c46b185a23d7e2087871e49ae608f83f9cacfa1f
Wachiraya  Phonsa <wachiraya@Wachirayas-MacBook-Air.local>
2025-03-10 17:02:42 +0700

    Update getPeople on Quantity

--- fe-coe-request-rooms/src/components/Quantity.tsx
+++ fe-coe-request-rooms/src/components/Quantity.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { PeopleData } from '../services/getPeople';
 
-interface Quantity {
-people: number;
+interface QuantityProps {
+  peopleData: PeopleData[];
 }
 
-const Quantity:React.FC<Quantity> = ({people}) => {
-  
+const Quantity: React.FC<QuantityProps> = ({ peopleData }) => {
   return (
-    <div className="flex flex-col items-center justify-center bg-white  shadow-sm rounded-2xl p-8 w-full space-y-2 h-full">
-      <div className="flex items-center justify-between">
-        <p className="text-lg font-medium">{people} People</p>
-      </div>
+    <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-2xl p-8 w-full space-y-2 h-full">
+      {peopleData.map((data, index) => (
+        <div key={index} className="flex items-center justify-between">
+          <p className="text-lg font-medium">{data.totalMovements} People</p>
+        </div>
+      ))}
       <p className="text-gray-500 font-medium text-lg">Quantity</p>
     </div>
   );
