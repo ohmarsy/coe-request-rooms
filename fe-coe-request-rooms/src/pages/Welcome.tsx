8adb671fe2630b879a81277eb80fe45c9f43ba59
WeerapongKh <weerapong.k@kkumail.com>
2025-03-10 22:26:50 +0700

    feat(Welcome): update page lay-out justify-between

--- fe-coe-request-rooms/src/pages/Welcome.tsx
+++ fe-coe-request-rooms/src/pages/Welcome.tsx
@@ -1,22 +1,24 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
-import ImgWelcome from '../assets/image-welcome-page.png';
-import Button from '../components/Button';
+import ImgWelcome from "../assets/image-welcome-page.png";
+import Button from "../components/Button";
 
-// Motion animation settings
-const motionSettings = {
-    initial: { opacity: 0, y: -100 },
+// Animation settings
+const fadeInUp = {
+    initial: { opacity: 0, y: -50 },
     animate: { opacity: 1, y: 0 },
-    transition: { duration: 0.8 }
+    transition: { duration: 0.8, ease: "easeOut" }
 };
 
 const WelcomePage = () => {
     return (
-        <motion.div {...motionSettings} className="w-screen h-screen  bg-[var(--background-color)] flex flex-col items-center text-[var(--text-color)]">
-            
+        <motion.div 
+            {...fadeInUp} 
+            className="h-screen flex flex-col items-center justify-between text-[var(--text-color)] bg-[var(--background-color)] px-4 gap-4"
+        >
             {/* Header */}
-            <h1 className="text-4xl md:text-6xl font-bold mb-8 mt-32 md:my-16">
+            <h1 className="text-4xl md:text-6xl font-bold mt-24">
                 <span className="text-[var(--primary-color)]">CoE</span> Rooms
             </h1>
 
@@ -26,18 +28,21 @@ const WelcomePage = () => {
             </Link>
 
             {/* Sign-up link */}
-            <p className="mt-8">
-                You don’t have an account?{' '}
-                <Link to="/sign-up" className="text-[var(--primary-color)] cursor-pointer underline">
+            <p className="text-sm">
+                Don’t have an account?{' '}
+                <Link to="/sign-up" className="text-[var(--primary-color)] underline">
                     Sign up
                 </Link>
             </p>
 
             {/* Welcome Image */}
-            <img className="mt-16" src={ImgWelcome} alt="Illustration of welcoming users to CoE Rooms" />
+            <img 
+                src={ImgWelcome} 
+                alt="Welcome illustration for CoE Rooms" 
+            />
 
             {/* Footer */}
-            <footer className="mt-32 text-center text-[#140B0C] opacity-40">
+            <footer className="w-full text-center text-[#140B0C] opacity-40 text-sm py-4">
                 <p>Developed by</p>
                 <p className="font-semibold">BOSS OHM LEE EARTH ICE</p>
             </footer>
