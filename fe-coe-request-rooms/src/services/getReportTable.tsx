8ed4ce1177d4056abd7310e98e9680a6424024f6
ohmarsy <suphachai.y@kkumail.com>
2025-03-11 12:34:21 +0700

    change the requirements.txt of auth service

--- fe-coe-request-rooms/src/services/getReportTable.tsx
+++ fe-coe-request-rooms/src/services/getReportTable.tsx
@@ -10,12 +10,12 @@ export interface ReportTableData {
     [key: string]: string; 
 }
 
-export const getReportTable = async (): Promise<ReportTableData[]> => {
-    try {
-        const response = await axios.get("http://localhost:5003/report-table");
-        return response.data;
-    } catch (err) {
-        console.error("Error fetching report table:", err);
-        throw err;
-    }
+// export const getReportTable = async (): Promise<ReportTableData[]> => {
+//     try {
+//         const response = await axios.get("http://localhost:5003/report-table/");
+//         return response.data;
+//     } catch (err) {
+//         console.error("Error fetching report table:", err);
+//         throw err;
+//     }
 }
