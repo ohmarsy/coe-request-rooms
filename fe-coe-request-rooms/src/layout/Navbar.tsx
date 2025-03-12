de4b788262b66e165015e6eae0db3f3065b03315
JakkapatB <jakkapat.bo@kkumail.com>
2025-03-12 15:45:45 +0700

    responsive nav bar

--- fe-coe-request-rooms/src/layout/Navbar.tsx
+++ fe-coe-request-rooms/src/layout/Navbar.tsx
@@ -14,6 +14,8 @@ const Navbar: React.FC<NavbarProps> = ({ toggleSidebar, isMobile }) => {
   const [userName, setUserName] = useState<User>();
   const [loading, setLoading] = useState(true);
 
+  const pathname = window.location.pathname;
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -59,34 +61,38 @@ const Navbar: React.FC<NavbarProps> = ({ toggleSidebar, isMobile }) => {
   }
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-[var(--primary-color)] h-16 flex items-center justify-between px-6 shadow-md z-50">
-      <div className="flex items-center gap-4">
+    <nav className="fixed top-0 left-0 w-full bg-[var(--primary-color)] h-16 flex items-center justify-between px-6 shadow-md z-50 ">
+      <div className="flex items-center gap-4 max-[500px]:gap-2">
         {isMobile && (
           <button onClick={toggleSidebar} className="text-white">
             <FontAwesomeIcon icon={faBars} size="lg" />
           </button>
         )}
         <img src="./src/assets/LogoCoE.png" className="w-12" alt="Logo" />
-        <span className="text-white text-xl font-bold">CoE Rooms</span>
+        <span className="text-white text-xl font-bold max-[500px]:text-sm">CoE Rooms</span>
       </div>
 
-      <div className="flex flex-row  items-center space-x-6">
-        <h1 className="text-white text-lg font-bold">
+      <div className="flex flex-row  items-center space-x-4 max-[500px]:space-x-2">
+        <h1 className="text-white text-lg font-bold max-[500px]:text-sm">
           {userName!.first_name ?? "User"}
         </h1>
-        <button
-          className="flex items-center gap-4 p-3 text-[var(--text-color)] w-full cursor-pointer"
-          onClick={() => {
-            localStorage.removeItem("access_token");
-            localStorage.removeItem("refresh_token");
-            window.location.reload();
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faRightFromBracket}
-            style={{ color: "white" }}
-          />
-        </button>
+        {pathname === "/request-rooms" ? (
+          <button
+            className="flex items-center gap-4 p-3 text-[var(--text-color)] w-full cursor-pointer"
+            onClick={() => {
+              localStorage.removeItem("access_token");
+              localStorage.removeItem("refresh_token");
+              window.location.reload();
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faRightFromBracket}
+              style={{ color: "white" }}
+            />
+          </button>
+        ) : (
+          null
+        )}
       </div>
     </nav>
   );
