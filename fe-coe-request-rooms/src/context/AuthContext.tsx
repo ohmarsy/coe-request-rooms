dd12565f49fd968de5deb0477cd7ebd40bc16e88
WeerapongKh <weerapong.k@kkumail.com>
2025-03-12 17:33:54 +0700

    feat: add interface role and store role in local-storage

--- fe-coe-request-rooms/src/context/AuthContext.tsx
+++ fe-coe-request-rooms/src/context/AuthContext.tsx
@@ -1,7 +1,9 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
+
 interface AuthContextType {
   isAuthenticated: boolean;
-  login: (accessToken: string, refreshToken: string) => void; // Accept tokens as parameters
+  role: string | null; // เพิ่มบทบาท
+  login: (accessToken: string, refreshToken: string, role: string) => void; // เพิ่ม role เป็นพารามิเตอร์
   logout: () => void;
 }
 
@@ -9,21 +11,26 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!localStorage.getItem('access_token'));
+  const [role, setRole] = useState<string | null>(localStorage.getItem('role')); // จัดเก็บบทบาทจาก localStorage
 
-  const login = (accessToken: string, refreshToken: string) => {
+  const login = (accessToken: string, refreshToken: string, role: string) => {
     localStorage.setItem('access_token', accessToken);
     localStorage.setItem('refresh_token', refreshToken);
+    localStorage.setItem('role', role); // จัดเก็บบทบาทใน localStorage
     setIsAuthenticated(true);
+    setRole(role);
   };
 
   const logout = () => {
     localStorage.removeItem('access_token');
     localStorage.removeItem('refresh_token');
+    localStorage.removeItem('role'); // ลบบทบาทจาก localStorage
     setIsAuthenticated(false);
+    setRole(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, role, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
