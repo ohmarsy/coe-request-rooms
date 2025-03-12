9c309bb675cfb31459c4bf5d10cd6a0ad98405de
WeerapongKh <weerapong.k@kkumail.com>
2025-03-12 16:39:54 +0700

    feat: implement getRooms function to fetch rooms data from API

--- fe-coe-request-rooms/src/services/getRooms.tsx
+++ fe-coe-request-rooms/src/services/getRooms.tsx
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import {api} from "./useApiService";
+
 import { RoomProps } from '../components/AllRoom';
 
 export const getRooms = async (): Promise<RoomProps[]> => {
   try {
-    const response = await axios.get("http://localhost:5003/rooms");
+    const response =await api.get("http://localhost:5003/rooms"); 
     
     return response.data;
   } catch (err) {
