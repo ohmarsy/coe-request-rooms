62507a3069225083f325da26667ae2b6cf2fb170
WeerapongKh <weerapong.k@kkumail.com>
2025-03-12 17:37:10 +0700

    feat(router): configure routing with role-based access control

--- fe-coe-request-rooms/src/routes/AppRouter.tsx
+++ fe-coe-request-rooms/src/routes/AppRouter.tsx
@@ -1,39 +1,39 @@
-// AppRouter.tsx
-import { Route, Routes, Navigate } from "react-router-dom";
-import WelcomePage from "../pages/Welcome";
-import SignUpPage from "../pages/SignUp";
-import SignInPage from "../pages/SignIn";
-import MainPage from "../pages/Main";
-import RequestRoomsPage from "../pages/RequestRooms";
-import ProtectedRoute from "../components/ProtectedRoute"; // นำเข้า ProtectedRoute
-const AppRouter = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<WelcomePage />} />
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
-      
-      {/* ใช้ ProtectedRoute สำหรับหน้า Main และ RequestRooms */}
-      <Route 
-        path="/main" 
-        element={
-          <ProtectedRoute>
-            <MainPage />
-          </ProtectedRoute>
-        } 
-      />
-      <Route 
-        path="/request-rooms" 
-        element={
-          <ProtectedRoute>
-            <RequestRoomsPage />
-          </ProtectedRoute>
-        } 
-      />
-      
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
-};
+  // AppRouter.tsx
+  import { Route, Routes, Navigate } from "react-router-dom";
+  import WelcomePage from "../pages/Welcome";
+  import SignUpPage from "../pages/SignUp";
+  import SignInPage from "../pages/SignIn";
+  import MainPage from "../pages/Main";
+  import RequestRoomsPage from "../pages/RequestRooms";
+  import ProtectedRoute from "../components/ProtectedRoute";
 
-export default AppRouter;
+  const AppRouter = () => {
+    return (
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        
+        <Route 
+          path="/main" 
+          element={
+            <ProtectedRoute allowedRoles={['staff']}>
+              <MainPage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/request-rooms" 
+          element={
+            <ProtectedRoute allowedRoles={['student', 'staff']}> 
+              <RequestRoomsPage />
+            </ProtectedRoute>
+          } 
+        />
+        
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    );
+  };
+
+  export default AppRouter;
