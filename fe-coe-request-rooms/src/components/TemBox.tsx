bbdd6efe4e823edc3784c3c4851780b24b732761
Wachiraya  Phonsa <wachiraya@Wachirayas-MacBook-Air.local>
2025-03-12 13:12:38 +0700

    Update syle TemBox

--- fe-coe-request-rooms/src/components/TemBox.tsx
+++ fe-coe-request-rooms/src/components/TemBox.tsx
@@ -24,27 +24,32 @@ const TemperatureDisplay: React.FC<TemperatureProps> = ({
 
   return (
     <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-2xl p-4 w-full h-full text-sm sm:text-base">
-      <div className="flex items-center">
-        <span className="text-2xl sm:text-5xl font-bold text-black">
-          {temperature}
-        </span>
-        <span className="text-base sm:text-3xl font-bold text-gray-400">
-          °{unit}
-        </span>
-      </div>
-      <div className="text-sm sm:text-base text-[#7d7d7d] mt-2">
-        {tempType === "inside" ? "Inside Temp" : "Outside Temp"}
-      </div>
-      {tempType === "inside" && (
-        <div className="text-xs xs:text-sm text-[#7d7d7d] mt-2">
-          Air conditioner:{" "}
-          <span
-            className={acStatus === "on" ? "text-green-500" : "text-red-500"}
-          >
-            {acStatus}
+      <div className="flex flex-col items-center min-h-[100px] justify-center"> 
+        <div className="flex items-center">
+          <span className="text-2xl sm:text-5xl font-bold text-black">
+            {temperature}
+          </span>
+          <span className="text-base sm:text-3xl font-bold text-gray-400">
+            °{unit}
           </span>
         </div>
-      )}
+        <div className="text-sm sm:text-base text-[#7d7d7d] mt-2">
+          {tempType === "inside" ? "Indoor Temp" : "Outdoor Temp"}
+        </div>
+        
+        <div className="text-xs xs:text-sm text-[#7d7d7d] mt-2 h-4">
+          {tempType === "inside" && (
+            <>
+              Air conditioner:{" "}
+              <span
+                className={acStatus === "on" ? "text-green-500" : "text-red-500"}
+              >
+                {acStatus}
+              </span>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
